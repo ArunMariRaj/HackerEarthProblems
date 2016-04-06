0c6d2c3360f5b0de6f8a0ab0e73478c068d3a880
ArunMariRaj <r.arunmr@gmail.com>
2016-04-07 02:20:46 +0530

    FindingEvenNumbers

---
+++ HackerEarthCoding/FindEvenNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FindEvenNumbers
+{
+    class FindEvenNumbers
+    {
+        static void Main(string[] args)
+        {
+            long n;
+            n = long.Parse(Console.ReadLine());
+            while (n != -1)
+            {
+                if (n % 2 == 0)
+                {
+                    Console.WriteLine("{0}", n);
+                }
+                n = long.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
