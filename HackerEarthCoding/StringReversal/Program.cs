c6549ce44a0bc8fc0a4c13f6b699a985f62de9d5
ArunMariRaj <r.arunmr@gmail.com>
2016-04-14 21:37:45 +0530

    Added StringReversal

---
+++ HackerEarthCoding/StringReversal/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringReversal
+{
+    class TerribleChandu
+    {
+        static void Main(string[] args)
+        {
+            TerribleChandu terribleChandu = new TerribleChandu();
+            string stringInput;
+            int numberOfStrings = int.Parse(Console.ReadLine());
+            while(numberOfStrings>0)
+            {
+                stringInput = Console.ReadLine();
+                Console.WriteLine("{0}",terribleChandu.ReverseString(stringInput));
+                numberOfStrings--;
+            }
+        }
+
+        public string ReverseString(string inputString)
+        {
+            char[] outputString = inputString.ToCharArray();
+            int stringLength = outputString.Length;
+            int reversalPivot = stringLength / 2;
+            char temp;
+            for (int i = 0; i < reversalPivot; i++)
+            {
+                stringLength--;
+                temp = outputString[i];
+                outputString[i] = outputString[stringLength];
+                outputString[stringLength] = temp;
+            }
+            return new string(outputString);
+        }
+    }
+}
